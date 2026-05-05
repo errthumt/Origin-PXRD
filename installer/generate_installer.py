aa735a15ad57bb23eb041ee720d5a196c72c1155
errthumt <errthumt@iastate.edu>
2026-05-05 17:05:49 -0500

    remove - indentation from installer link lines

--- installer/generate_installer.py
+++ installer/generate_installer.py
@@ -170,8 +170,8 @@ def update_recent_links(version: str):
         zipf = zip_url(nest_level)
         return (
             f"## Release {get_base_version(version)}\n\n"
-            f"- Installer: [OriginPXRD_Installer_v{version}.exe]({inst})\n"
-            f"- Zip Package: [OriginPXRD_v{version}.zip]({zipf})\n"
+            f"Installer: [OriginPXRD_Installer_v{version}.exe]({inst})\n"
+            f"Zip Package: [OriginPXRD_v{version}.zip]({zipf})\n"
             f"<!--end release link-->"
         )
 
